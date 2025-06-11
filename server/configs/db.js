7b24f29ae562f4196bef57d1335c5fbd22e121bc
Sachin1829Rajguru <sdccrajguru@gmail.com>
2025-06-11 16:33:02 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+        mongoose.set('bufferCommands', false);
         mongoose.connection.on("connected", () =>
             console.log("MongoDB connected.")
         );
